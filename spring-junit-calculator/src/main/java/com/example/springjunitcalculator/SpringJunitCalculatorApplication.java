4745a8ba752ff9a09814049dc6b590f0fafaa7d8
VincePlantItAi <61615301+JooHyukKim@users.noreply.github.com>
2021-08-17 19:01:30 +0900

    FIX : learnKotlin 폴더 네이밍 올바르게 수정

---
+++ spring-junit-calculator/src/main/java/com/example/springjunitcalculator/SpringJunitCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.springjunitcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJunitCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJunitCalculatorApplication.class, args);
+    }
+
+}
---
+++ spring-junit-calculator/src/test/java/com/example/springjunitcalculator/SpringJunitCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springjunitcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJunitCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
