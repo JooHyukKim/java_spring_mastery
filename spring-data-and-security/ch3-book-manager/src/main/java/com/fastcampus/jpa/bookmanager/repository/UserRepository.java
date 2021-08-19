4981fe1780599cc390025045952e915ac17e6ce7
VincePlantItAi <61615301+JooHyukKim@users.noreply.github.com>
2021-08-19 20:41:53 +0900

    FEAT : 쿼리메소드 기본실습

--- spring-data-and-security/ch3-book-manager/src/main/java/com/fastcampus/jpa/bookmanager/repository/UserRepository.java
+++ spring-data-and-security/ch3-book-manager/src/main/java/com/fastcampus/jpa/bookmanager/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.fastcampus.jpa.bookmanager.repository;
 import com.fastcampus.jpa.bookmanager.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -33,8 +34,39 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     List<User> findTop2ByName(String email);
 
+    List<User> findByEmailAndName(String email, String name);
 
-//    List<User> findByName(String name);
-//    Set<User> findByName(String name);
+    List<User> findByEmailOrName(String email, String name);
+
+    List<User> findByCreatedAtAfter(LocalDateTime yesterday);
+
+    List<User> findByCreatedAtGreaterThan(LocalDateTime yesterday);
+
+    List<User> findByCreatedAtGreaterThanEqual(LocalDateTime yesterday);
+
+    List<User> findByCreatedAtBetween(LocalDateTime yesterday, LocalDateTime tommorow);
+
+    List<User> findByIdBetween(long start, long finish);
+
+    List<User> findByIdIsNot(long start, long finish);
+
+    List<User> findByIdGreaterThanEqualAndIdLessThanEqual(long start, long finish);
+
+    List<User> findByIdNotEmpty(String id);
+
+    List<User> findByIdNotNull(String id);
+
+    List<User> findByNameStartingWith(String name);
+
+    List<User> findByNameEndingWith(String name);
+
+    List<User> findByNameContaining(String name);
+
+    List<User> findByNameLike(String name);
+
+    Optional<User> findUserByName(String name);
+    Optional<User> findUserByNameEquals(String name);
+    Optional<User> findUserByNameIs(String name);
+
 
 }
--- spring-data-and-security/ch3-book-manager/src/test/java/com/fastcampus/jpa/bookmanager/repository/UserRepositoryTest.java
+++ spring-data-and-security/ch3-book-manager/src/test/java/com/fastcampus/jpa/bookmanager/repository/UserRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.fastcampus.jpa.bookmanager.repository;
 
 import com.fastcampus.jpa.bookmanager.domain.User;
+import jdk.vm.ci.meta.Local;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,6 +10,7 @@ import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers.contains;
@@ -24,20 +26,42 @@ class UserRepositoryTest {
     private UserRepository userRepository;
 
     @Test
+    void crud4() {
+        String martinEmail = "martin@fastcampus.com";
+        String martinName = "martin";
+        System.out.println(userRepository.findByEmailAndName(martinEmail, martinName));
+        System.out.println(userRepository.findByEmailOrName(martinEmail, martinName));
+        System.out.println(userRepository.findByCreatedAtAfter(LocalDateTime.now().minusDays(1L)));
+        System.out.println(userRepository.findByCreatedAtBetween(LocalDateTime.now().minusDays(1L), LocalDateTime.now().plusDays(1L)));
+
+        System.out.println("findByIdGreaterThanEqualAndIdLessThanEqual " + userRepository.findByIdGreaterThanEqualAndIdLessThanEqual(1,3));
+        System.out.println("findByIdBetween" + userRepository.findByIdBetween(1,3));
+
+        System.out.println("findByNameLike" + userRepository.findByNameLike("%" + "rti" + "%"));
+
+
+    }
+
+    @Test
     void crud5() {
-        System.out.println("findByEmail : " + userRepository.findByEmail("martin@fastcampus.com"));
-        System.out.println("getByEmail : " + userRepository.getByEmail("martin@fastcampus.com"));
-        System.out.println("readByEmail : " + userRepository.readByEmail("martin@fastcampus.com"));
-        System.out.println("searchByEmail : " + userRepository.searchByEmail("martin@fastcampus.com"));
-        System.out.println("queryByEmail : " + userRepository.queryByEmail("martin@fastcampus.com"));
+        String martinEmail = "martin@fastcampus.com";
+        String martinName = "martin";
+        System.out.println("findByEmail : " + userRepository.findByEmail(martinEmail));
+        System.out.println("getByEmail : " + userRepository.getByEmail(martinEmail));
+        System.out.println("readByEmail : " + userRepository.readByEmail(martinEmail));
+        System.out.println("searchByEmail : " + userRepository.searchByEmail(martinEmail));
+        System.out.println("queryByEmail : " + userRepository.queryByEmail(martinEmail));
 
-        System.out.println(userRepository.findSomethingByEmail("martin@fastcampus.com"));
+        System.out.println(userRepository.findSomethingByEmail(martinEmail));
 
-        System.out.println(userRepository.findFirst1ByName("martin"));
-        System.out.println(userRepository.findTop1ByName("martin"));
+        System.out.println(userRepository.findFirst1ByName(martinName));
+        System.out.println(userRepository.findTop1ByName(martinName));
 
-        System.out.println(userRepository.findFirst2ByName("martin"));
-        System.out.println(userRepository.findTop2ByName("martin"));
+        System.out.println(userRepository.findFirst2ByName(martinName));
+        System.out.println(userRepository.findTop2ByName(martinName));
+
+        System.out.println(userRepository.findByEmailAndName(martinEmail, martinName));
+        System.out.println(userRepository.findByEmailOrName(martinEmail, martinName));
 
     }
 }
